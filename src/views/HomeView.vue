139e1d9bbd03b906c333a9d1d1ae596e98fd5fef
JCarvajalLab <carvajal.jordan94@gmail.com>
2024-11-25 18:41:33 -0300

    Se logra avanzar y dejar las rutas operativas, luego se continuara con la api para finalizar el trabajo

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="home fondo">
+    <div >
+    
+    </div>
+
+    
+
+
+
+    </div>
+</template>
+
+<script>
+export default  {
+  name: 'HomeViews',
+  components: {}
+}
+</script>
+
+<style>
+.fondo {
+    background-image: url("https://brandemia.org/contenido/subidas/2021/05/portada-starwars-imagenes-brandemia-blog-1000x670.jpg");
+    width: 70%;
+    height: 80%;
+    display: block;
+    margin: 0px auto;
+}
+</style>
